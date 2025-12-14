951ebf52ffc5f12a7007ecaf9c5d4e7645db3bb9
0xC0DE666 <0xC0DE666@proton.me>
2025-12-14 12:30:42 +0200

    impl ToSql and FromSql for sql::Value

--- src/sql.rs
+++ src/sql.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 pub const NULL: &str = "null";
 
 #[derive(Clone, Debug, PartialEq)]
@@ -13,3 +15,46 @@ pub enum Value {
     /// The value is a blob of data
     Blob(Vec<u8>),
 }
+
+// RUSQLITE
+impl rusqlite::types::ToSql for Value {
+    fn to_sql(&self) -> rusqlite::Result<rusqlite::types::ToSqlOutput<'_>> {
+        let val = match self {
+            Value::Null => rusqlite::types::ValueRef::Null,
+            Value::Integer(i) => rusqlite::types::ValueRef::Integer(*i),
+            Value::Real(r) => rusqlite::types::ValueRef::Real(*r),
+            Value::Text(t) => rusqlite::types::ValueRef::Text(t.as_bytes()),
+            Value::Blob(b) => rusqlite::types::ValueRef::Blob(b),
+        };
+
+        Ok(rusqlite::types::ToSqlOutput::Borrowed(val))
+    }
+}
+
+impl rusqlite::types::FromSql for Value {
+    fn column_result(value: rusqlite::types::ValueRef<'_>) -> rusqlite::types::FromSqlResult<Self> {
+        let val = match value {
+            rusqlite::types::ValueRef::Null => Value::Null,
+            rusqlite::types::ValueRef::Integer(i) => Value::Integer(i),
+            rusqlite::types::ValueRef::Real(r) => Value::Real(r),
+            rusqlite::types::ValueRef::Text(t) => Value::Text(
+                String::from_utf8(t.into())
+                    .map_err(|_| rusqlite::types::FromSqlError::InvalidType)?,
+            ),
+            rusqlite::types::ValueRef::Blob(b) => Value::Blob(b.into()),
+        };
+
+        Ok(val)
+    }
+}
+
+// SQLX
+// impl<'q> sqlx::Encode<'q, sqlx::Sqlite> for Value {
+//     fn encode_by_ref(
+//         &self,
+//         args: &mut Vec<sqlx::sqlite::SqliteArgumentValue<'q>>,
+//     ) -> Result<sqlx::encode::IsNull, Box<dyn Error>> {
+//         args.push(sqlx::sqlite::SqliteArgumentValue::Int64(self.epoch));
+//         Ok(sqlx::encode::IsNull::No)
+//     }
+// }
